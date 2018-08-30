b7ca5e001409d13ed27c7aa371d5ffc01b217871
choigone <goun4439@gmail.com>
2018-08-30 17:51:58 +0900

    Halmate page View 완료

--- HalAe_goun/app/src/main/java/com/team/halae/MainActivity.kt
+++ HalAe_goun/app/src/main/java/com/team/halae/MainActivity.kt
@@ -2,11 +2,22 @@ package com.team.halae
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addFragment(HalmateActivity())
+    }
+
+    fun addFragment(fragment: Fragment){
+        val fm = supportFragmentManager
+        val transaction = fm.beginTransaction()
+        transaction.add(R.id.main_frame, fragment)
+        transaction.commit()
     }
 }
